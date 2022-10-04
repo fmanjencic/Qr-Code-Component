c77235f878c7a79fe375cf19ec72c2220aeff1f0
Filip Manjencic <76404759+fmanjencic@users.noreply.github.com>
2022-10-04 00:04:27 -0400

    updating

--- src/Qr.js
+++ src/Qr.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Qr.css';
+import Qrimg from './image-qr-code.png'
 
 function Qr() {
   return (
-    <div>
-        <h1>Testing It Now</h1>
+    <div className='container'>
+        <img src={Qrimg} title="Qr Code" alt="QR Code"/>
+        <h1>Test</h1>
+        <p>Improve your front-end skills by building project</p>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
   )
 }
